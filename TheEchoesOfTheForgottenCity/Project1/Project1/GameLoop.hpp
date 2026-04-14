4b01d0c25e702ec8e892c923c7a96087e6fe416f
aanikaa2325-bit <aanikaa2325@gmail.com>
2026-04-14 19:08:05 +0600

    Update GameLoop.hpp

--- TheEchoesOfTheForgottenCity/Project1/Project1/GameLoop.hpp
+++ TheEchoesOfTheForgottenCity/Project1/Project1/GameLoop.hpp
@@ -4,7 +4,7 @@
 #include "GameVariables.hpp"
 #include "Utilities.hpp"
 #include "Audio.hpp"
-#include "Level1.hpp" // For LoadLevel1, UpdateBubbles, UpdateFirePillar, iScreenShak
+#include "Level1.hpp" // For LoadLevel1, UpdateBubbles, UpdateFirePillar, iScreenShake
 #include "Slideshow.hpp" // For UpdateSlideshow, SkipSlide
 #include "Puzzles.hpp" // For UpdatePuzzle, UpdateFlowPuzzle, InitPuzzle, InitFlowPuzzle
 #include "SaveLoad.hpp"
